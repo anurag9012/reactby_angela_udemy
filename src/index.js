476b9638bd9a02922bad3e708b1761583c90421a
anurag9012 <88288329+anurag9012@users.noreply.github.com>
2022-02-02 23:16:33 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import reactdom from "react-dom";
+
+reactdom.render(
+  <div>
+    {" "}
+    <h1>hello</h1>
+    <ol>
+      {" "}
+      <li>ladoo</li>
+      <li>asdfsf</li>
+      <li>sfadsff</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
